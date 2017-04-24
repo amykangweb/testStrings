d545a73d75f7f4d8b6c6d1135d1bf42019aa8cf8
Amy Kang <amykang@Amys-MacBook-Air.local>
2017-04-24 02:08:21 -0700

    if else and switch statements.

--- testStrings/main.m
+++ testStrings/main.m
@@ -6,14 +6,90 @@
 //  Copyright © 2017 Amy Kang. All rights reserved.
 //
 
+enum popcornSizes {
+    kidsPopcorn = 1,
+    smallPopcorn = 2,
+    mediumPopcorn = 3,
+    largePopcorn = 4,
+    tubPopcorn = 5
+};
+
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
+        // insert code here...7815
         NSLog(@"Hello, World!");
     }
     
+    // Reg Price $10
+    // Senior Price $5
+    // Matinee price $4
+    
+    /*
+    bool isMatinee = NO;
+    
+    float regPrice = 10;
+    float seniorPrice = 5;
+    float matPrice = 4;
+    
+    int minAge = 60;
+    int custAge = 75;
+    
+    float custPrice;
+    
+    if(isMatinee) {
+        
+        custPrice = matPrice;
+        
+    } else if(custAge >= minAge){
+        
+        custPrice = seniorPrice;
+        
+    }else {
+        
+        custPrice = regPrice;
+    }
+     */
+    
+    // Popcorn sizes and prices
+    // Kids - $1.50
+    // Small - $3.00
+    // Medium - $4.25
+    // Large - $5.25
+    // Tub - $6.00
+    
+    float popcornPrice;
+    int popcornSize = mediumPopcorn;
+    
+    switch(popcornSize) {
+        case kidsPopcorn:
+            popcornPrice = 1.5;
+            break;
+            
+        case smallPopcorn:
+            popcornPrice = 3;
+            break;
+            
+        case mediumPopcorn:
+            popcornPrice = 4.25;
+            break;
+            
+        case largePopcorn:
+            popcornPrice = 5.25;
+            break;
+            
+        case tubPopcorn:
+            popcornPrice = 6;
+            break;
+            
+        default:
+            NSLog(@"No valid size entered");
+            break;
+        
+    }
+    
+    /*
     int apples = 3 + 5 + 4 + 1;
     
     int oranges = 10 - 3;
@@ -25,7 +101,11 @@ int main(int argc, const char * argv[]) {
     int baskets = 4;
     
     float itemsPerBasket = (apples + oranges + eggs) / baskets;
+     */
     
+    bool compareResult;
+    
+    compareResult = 5 != 5.75;
     
     bool hasLicense;
     hasLicense = true;
